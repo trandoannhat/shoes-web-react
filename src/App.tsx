188b6247307e0a9401cf419d519ee6ea43e74594
Trần Doãn Nhất <trandoannhat@yahoo.com>
2025-12-30 19:53:38 +0700

    create project and Homepage

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-8xl font-bold text-center mt-20 text-red-500'>
+        Tailwindcss oke!  
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div className="text-center text-4xl font-bold mt-20">Home Page</div>;
+}
+
+export default Home;
